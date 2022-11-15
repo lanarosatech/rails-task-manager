97d3fb27fd3f775d7ef629cdf887e0b02f56c764
lana freitas <lanadeoliveira@gmail.com>
2022-11-15 17:50:03 -0300

    done

--- db/migrate/20221115125525_create_task.rb
+++ db/migrate/20221115125525_create_task.rb
@@ -1,9 +1,9 @@
 class CreateTask < ActiveRecord::Migration[7.0]
   def change
-    create_table :task do |t|
+    create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.boolean :completed == false
+      t.boolean :completed, default: false
 
       t.timestamps
     end
